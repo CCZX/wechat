e562a6c2dc1d0bd1cda52d36fc6fa6c0c804b0b8
CCZX <1831058205@qq.com>
2020-01-29 22:13:05 +0800

    client: 添加常量

---
+++ chatClient/src/store/constants.js
@@ -0,0 +1,5 @@
+export const SET_UNREAD_NEWS_TYPE_MAP = {
+  add: 'ADD',
+  clear: 'CLEAR',
+  replace: 'REPLACE'
+}
--- chatClient/src/store/index.js
+++ chatClient/src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import VueX from 'vuex'
 
 import user from './modules/user'
+import news from './modules/news'
 
 Vue.use(VueX)
 
 export default new VueX.Store({
   modules: {
-    user
+    user,
+    news
   }
 })
