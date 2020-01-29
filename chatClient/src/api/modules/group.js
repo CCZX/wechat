5e33bddf1e7bd620a154c752c0fcfa3222ab792b
CCZX <1831058205@qq.com>
2020-01-29 22:19:11 +0800

    client: API add comment

--- chatClient/src/api/modules/group.js
+++ chatClient/src/api/modules/group.js
@@ -1,10 +1,18 @@
 import request from '@/utils/request'
 
 export default {
+  /**
+   * 根据用户名获取其对应的群聊，用户名在user实体中对应name字段
+   * @param {Object} data 
+   */
   getMyGroup(data) {
     let { userName } = data
     return request.get(`/group/getmygroup?userName=${userName}`)
   },
+  /**
+   * 根据id获取群聊详情
+   * @param {*} data 
+   */
   getGroupInfo(data) {
     let { id } = data
     return request.get(`/group/groupinfo?id=${id}`)
