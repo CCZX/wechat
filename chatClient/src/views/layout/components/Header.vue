b3809d35b72ee79be7ddd0b6a55ca5d585aec023
CCZX <1831058205@qq.com>
2020-02-03 20:55:50 +0800

    client: 增加新的列表功能 记录未读的系统消息

--- chatClient/src/views/layout/components/Header.vue
+++ chatClient/src/views/layout/components/Header.vue
@@ -12,18 +12,47 @@
           </router-link>
         </span>
         <span class="item">
-          <router-link to="/add" tag="span">
+          <router-link to="/mzone" tag="span">
             发动态 <i class="el-icon-plus"></i>
           </router-link>
         </span>
       </div>
       <div class="user-info">
-        <el-avatar size="large"
-          :src="'http://localhost:3333' + userInfo.photo"
-          @error="() => true"
-        >
-          <img src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt="" srcset="">
-        </el-avatar>
+        <el-dropdown class="droplist">
+          <span class="el-dropdown-link">
+            <el-badge :value="validateUnReadCount" class="badge-item">
+              <el-avatar size="large"
+                class="avatar"
+                :src="'http://localhost:3333' + userInfo.photo"
+                @error="() => true"
+              >
+                <img src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt="" srcset="">
+              </el-avatar>
+            </el-badge>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item class="user-menu-item">
+              <router-link to="/setting" class="link">
+                个人设置
+              </router-link>
+            </el-dropdown-item>
+            <el-dropdown-item class="user-menu-item">
+              <router-link to="/system" class="link">
+                系统消息
+              </router-link>
+            </el-dropdown-item>
+            <el-dropdown-item class="user-menu-item">
+              <router-link to="/setting" class="link">
+                主题设置
+              </router-link>
+            </el-dropdown-item>
+            <el-dropdown-item class="user-menu-item">
+              <router-link to="/setting" class="link">
+                反馈
+              </router-link>
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
         <div class="name-sigin">
           <span class="name">{{userInfo.nickname}}</span>
           <span class="sigin">{{userInfo.signature}}</span>
@@ -38,17 +67,35 @@ import { mapState } from 'vuex'
 export default {
   data() {
     return {
-      userInfo: {}
+      
     }
   },
-  created() {
-    this.userInfo = this.$store.state.user.userInfo
+  computed: {
+    ...mapState('user', {
+      userInfo: 'userInfo'
+    }),
+    ...mapState('news', {
+      unreadNews: 'unreadNews'
+    }),
+    validateUnReadCount() {
+      const validateSysUser = this.$store.state.app.sysUsers.filter(item => item.code === '111111')[0]
+      const key = validateSysUser._id + '-' + this.userInfo._id
+      return this.unreadNews[key]
+    }
   },
 }
 </script>
 
 
 <style lang="scss">
+.user-menu-item {
+  .link {
+    text-decoration: none;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
 .layout-header__com {
   @import "./../../../../static/css/var.scss";
   .el-header {
@@ -92,6 +139,15 @@ export default {
     .user-info {
       display: flex;
       align-items: center;
+      .droplist {
+        height: 60px;
+        .el-badge__content.is-fixed {
+          top: 17px;
+        }
+        .avatar {
+          margin-top: 10px;
+        }
+      }
       .name-sigin {
         display: flex;
         flex-direction: column;
