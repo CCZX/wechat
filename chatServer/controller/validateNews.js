c83f35c84397bfc547373540ac0484324a21acdf
CCZX <1831058205@qq.com>
2020-02-03 20:46:12 +0800

    server: 将systemnews表改为validatenews表，并对validatenews数据库做一些操作

---
+++ chatServer/controller/validateNews.js
@@ -0,0 +1,51 @@
+const VALIDATE_NEWS = require('../models/validateNews')
+
+const insertValidateNews = async (news) => {
+  const data = await VALIDATE_NEWS.insertMany(news)
+  console.log(data)
+}
+
+const findMyValidateNews = async (req, res) => {
+  const { id } = req.query
+  try {
+    const data = await VALIDATE_NEWS.find({
+      $or: [
+        {senderId: id}, {reveiverId: id}
+      ]
+    })
+    return res.json({
+      status: 2000,
+      data,
+      msg: '获取数据成功！'
+    })
+  } catch (error) {
+    res.json({
+      status: 2003,
+      data: error,
+      msg: '服务端错误，请稍后重试！'
+    })
+  }
+}
+
+/**
+ * 在被申请者对验证消息做出响应后改变消息的status状态：0/1/2，未处理/同意/不同意
+ */
+const changeValidateNewsStatus = async (data, status) => {
+  const { roomid, senderId, receiverId } = data
+  const doc = await VALIDATE_NEWS.updateMany({
+    roomid,
+    senderId,
+    receiverId
+  }, {
+    $set: {
+      status: Number(status)
+    }
+  })
+  console.log(doc)
+}
+
+module.exports = {
+  insertValidateNews,
+  findMyValidateNews,
+  changeValidateNewsStatus
+}
---
+++ chatServer/models/validateNews.js
@@ -0,0 +1,26 @@
+// 系统用户数据模型
+const DB = require('../utils/connectDB')
+const Schema = DB.Schema
+
+const validateNewsSchema = new Schema({
+  roomid: String,
+  senderId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  senderName: String,
+  senderNickname: String, // 发送者昵称
+  senderAvatar: String, // 发送者头像
+  reveiverId: { // 接收者ID
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  time: String, // 消息发送时间
+  additionMessage: String, // 附加消息
+  status: Number, // 0/1/2，未处理/同意/不同意
+  validateType: Number, /// 0/1, 好友/群聊
+})
+
+const validateNews = DB.model('validateNews', validateNewsSchema)
+
+module.exports = validateNews
