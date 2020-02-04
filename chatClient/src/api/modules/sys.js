d4521e8d61b7006561ff40fd152aa9f2c675cbfc
CCZX <1831058205@qq.com>
2020-02-04 21:10:28 +0800

    client: asynchronous server API

--- chatClient/src/api/modules/sys.js
+++ chatClient/src/api/modules/sys.js
@@ -3,5 +3,8 @@ import request from '@/utils/request'
 export default {
   getSysUsers() {
     return request.get('/sys/sysusers')
+  },
+  getQiniuToken() {
+    return request.get('/sys/qiniutoken')
   }
 }
