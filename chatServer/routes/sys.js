957122c8137e28c84d23d142a7a86426753e6e8a
CCZX <1831058205@qq.com>
2020-02-04 21:03:15 +0800

    server: 添加获取七牛云token接口

--- chatServer/routes/sys.js
+++ chatServer/routes/sys.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const qiniu = require('qiniu')
+const { ACCESS_KEY, SECRET_KEY, BUCKET } = require('./../utils/config')
 const sysUserController = require('./../controller/systemUser')
 
 router.get('/sysusers', sysUserController.getSysUser)
+
+router.get('/qiniutoken', async (req, res) => {
+  const mac = new qiniu.auth.digest.Mac(ACCESS_KEY, SECRET_KEY)
+  const options = {
+    scope: BUCKET
+  }
+  const putPolicy = new qiniu.rs.PutPolicy(options)
+  const token = putPolicy.uploadToken(mac)
+  res.json({
+    status: 2000,
+    data: token,
+    msg: 'success'
+  })
+})
 
 module.exports = router
