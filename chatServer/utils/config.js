edee714fc2e4184dfad959ab16f864b133d6686e
CCZX <1831058205@qq.com>
2020-02-04 21:05:58 +0800

    server: 七牛云AK和SK

---
+++ chatServer/utils/config.js
@@ -0,0 +1,9 @@
+const ACCESS_KEY = 'frWfpDLauQSLS9pLI5fIQEGTxMFnpqfHCrZXn1da'
+const SECRET_KEY = 'otLFopTxILdZf8KX9hg1TCTUB1hXj9_SdhKrq1P_'
+const BUCKET = 'chenchao'
+
+module.exports = {
+  ACCESS_KEY,
+  SECRET_KEY,
+  BUCKET
+}
