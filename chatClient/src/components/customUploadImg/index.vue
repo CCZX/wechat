7f0c100f968333b952a678e6bfd4a54fdb9141e5
CCZX <1831058205@qq.com>
2020-02-05 12:29:00 +0800

    client: modify CSS

--- chatClient/src/components/customUploadImg/index.vue
+++ chatClient/src/components/customUploadImg/index.vue
@@ -97,6 +97,7 @@ export default {
         height: 30px;
         line-height: 30px;
         text-align: center;
+        cursor: pointer;
         &::before {
           margin-right: 5px;
         }
