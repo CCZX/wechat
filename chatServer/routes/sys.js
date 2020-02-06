d0ab97fd7f3ea1c4e3c18f16355564ca27c01031
CCZX <1831058205@qq.com>
2020-02-06 19:11:45 +0800

    server: 添加获取系统使用情况API

--- chatServer/routes/sys.js
+++ chatServer/routes/sys.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const qiniu = require('qiniu')
+const OSUtils = require('os-utils')
+const OS = require('os')
 const { ACCESS_KEY, SECRET_KEY, BUCKET } = require('./../utils/config')
 const sysUserController = require('./../controller/systemUser')
 
@@ -17,6 +19,26 @@ router.get('/qiniutoken', async (req, res) => {
     status: 2000,
     data: token,
     msg: 'success'
+  })
+})
+
+router.get('/syssituation', (req, res) => {
+  let freeMem = OS.freemem() / 1024 / 1024 / 1024
+  let totalMem = OS.totalmem() / 1024 / 1024 / 1024
+  OSUtils.cpuUsage((val) => {
+    let curCPU = val
+    const data = {
+      cpuUsage: ( curCPU * 10 ).toFixed(2),
+      freeMem: freeMem.toFixed(2) + "G",
+      totalMem: totalMem.toFixed(2) + "G",
+      usedMem: (totalMem - freeMem).toFixed(2) + "G",
+      MemUsage: ( (totalMem - freeMem)/totalMem * 10 ).toFixed(2),
+    }
+    return res.json({
+      status: 2000,
+      data,
+      msg: 'Success'
+    })
   })
 })
 
