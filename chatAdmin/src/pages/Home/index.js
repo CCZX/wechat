381820cea0403d2c06464698b2182e52abdc52fd
CCZX <1831058205@qq.com>
2020-02-06 19:09:17 +0800

    admin:增加home功能

--- chatAdmin/src/pages/Home/index.js
+++ chatAdmin/src/pages/Home/index.js
@@ -1,28 +1,52 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Carousel } from 'antd'
+import { systemApi } from './../../api'
+import DashBoard from './components/DashBoard'
 import './index.scss'
 
-class Home extends Component {
-  render() {
-    return (
-      <div className="home-page">
-        <Carousel autoplay>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-        </Carousel>,
+function Home() {
+  const [systemInfo, setSystemInfo] = useState({
+    cpuUsage: 0,
+    MemUsage: 0
+  })
+  useEffect(() => {
+    systemApi.getSystemInfo().then(res => {
+      const { data } = res
+      if (data.status === 2000) {
+        setSystemInfo(data.data)
+      }
+    })
+  }, [])
+  return (
+    <div className="home-page">
+      <div className="system-info">
+        <div className="item">
+          <DashBoard source={systemInfo.cpuUsage} font="CPU使用率" />
+        </div>
+        <div className="item">
+          <DashBoard source={systemInfo.MemUsage} font="内存使用率" />
+        </div>
+        {/* <div className="item">
+          <DashBoard />
+        </div> */}
       </div>
-    )
-  }
+      <Carousel autoplay>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+      </Carousel>
+    </div>
+
+  )
 }
 
 export default Home
