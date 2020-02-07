a312692af80d71d320f4d56f243f006281a6b028
CCZX <1831058205@qq.com>
2020-02-07 19:00:08 +0800

    client: 在会话中添加触发CoArtBoard的功能

--- chatClient/src/views/chat/components/Header.vue
+++ chatClient/src/views/chat/components/Header.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="chat-area__header">
     <transition name="roll">
-      <div class="wrapper" v-if="currentConversation.roomid">
-        <span>{{currentConversation.isGroup ? currentConversation.groupId.title : currentConversation.nickname}}</span>
-        <i class="icon-qun iconfont iconic iconic-group" v-if="currentConversation.conversationType === 'GROUP'"></i>
-        <i class="el-icon-user-solid iconic " v-else></i>
+      <div class="header-wrapper" v-if="currentConversation.roomid">
+        <div class="header-title">
+          <span>{{currentConversation.isGroup ? currentConversation.groupId.title : currentConversation.nickname}}</span>
+          <i class="icon-qun iconfont iconic iconic-group" v-if="currentConversation.conversationType === 'GROUP'"></i>
+          <i class="el-icon-user-solid iconic " v-else></i>
+        </div>
+        <div class="header-operation">
+          <el-tooltip class="item" effect="dark" content="白板协作需要良好的网络环境" placement="top">
+            <i class="operation-item iconfont icon-huaban" @click="enterArtBoard"></i>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="视频通话需要良好的网络环境" placement="top">
+            <i class="operation-item iconfont icon-shipin"></i>
+          </el-tooltip>
+        </div>
       </div>
     </transition>
   </div>
@@ -15,7 +25,22 @@ import './../../../../static/iconfont/iconfont.css'
 export default {
   props: {
     currentConversation: Object
-  }
+  },
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    userInfo() {
+      return this.$store.state.user.userInfo
+    }
+  },
+  methods: {
+    enterArtBoard() {
+      this.$store.dispatch('app/SET_ISTOCOARTBOARD')
+    }
+  },
 }
 </script>
 
@@ -24,9 +49,19 @@ export default {
   box-sizing: border-box;
   height: 60px;
   padding: 0 10px;
-  // background-color: rgba(0, 0, 0, .3);
   line-height: 60px;
   border-bottom: 1px solid #cccccc;
+  .header-wrapper {
+    display: flex;
+    justify-content: space-between;
+    .header-operation {
+      .operation-item {
+        font-size: 20px;
+        margin-left: 10px;
+        cursor: pointer;
+      }
+    }
+  }
   .iconic-group::before {
     font-size: 20px;
   }
