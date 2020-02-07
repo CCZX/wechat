80aa9782334382e352aac522da0869863b2ffd89
CCZX <1831058205@qq.com>
2020-02-07 19:03:20 +0800

    client: vuex add new state

--- chatClient/src/store/modules/app.js
+++ chatClient/src/store/modules/app.js
@@ -1,5 +1,7 @@
 const state = {
-  sysUsers: '' || JSON.parse(window.localStorage.getItem('sysusers'))
+  sysUsers: '' || JSON.parse(window.localStorage.getItem('sysusers')),
+  isToCoArtBoard: false,
+  currentConversation: {}
 }
 
 const mutations = {
@@ -8,12 +10,24 @@ const mutations = {
     state.sysUsers = data
     const dataStr = JSON.stringify(data)
     window.localStorage.setItem('sysusers', dataStr)
+  },
+  setIsToCoArtBoard(state, data) {
+    state.isToCoArtBoard = !state.isToCoArtBoard
+  },
+  setCurrentConversation(state, data) {
+    state.currentConversation = data
   }
 }
 
 const actions = {
   SET_SYS_USERS({commit}, data) {
     commit('setSysUsers', data)
+  },
+  SET_ISTOCOARTBOARD({commit}, data) {
+    commit('setIsToCoArtBoard', data)
+  },
+  SET_CURRENT_CONVERSATION({commit}, data) {
+    commit('setCurrentConversation', data)
   }
 }
 
