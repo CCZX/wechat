68854f656cfc91d9d80b371f9ac2bf9570c2ded3
CCZX <1831058205@qq.com>
2020-02-07 19:04:52 +0800

    client: 添加处理CoArtBoard组件中使用的常量

--- chatClient/src/const/index.js
+++ chatClient/src/const/index.js
@@ -7,6 +7,7 @@ export const APP_VERSION = '1.0.0'
 
 export const VueAMapKey = '704b3b5e984fa3b6185c3f012da42547'
 
+// 在添加好友或者群聊时对应的搜索条件
 export const searchObjectMap = [
   {id: 1, label: '人', value: 'friend'},
   {id: 2, label: '群', value: 'group'}
@@ -31,8 +32,29 @@ export const validateNewsTips = {
 
 export const qiniu_URL = 'http://blog.static.chenr.cn/'
 
+// 七牛云上传图片时的状态
 export const uploadImgStatusMap = {
   error: 'error',
   next: 'next',
   complete: 'complete'
+}
+
+// 在CoArtBoard组件中对调色板的操作
+export const coArtBoardHandleOption = [
+  { name: "圆", type: "arc" },
+  { name: "线条", type: "line" },
+  { name: "矩形", type: "rect" },
+  { name: "多边形", type: "polygon" },
+  { name: "橡皮擦", type: "eraser" },
+  { name: "撤回", type: "cancel" },
+  { name: "前进", type: "go" },
+  { name: "清屏", type: "clear" },
+  { name: "线宽", type: "lineWidth" },
+  { name: "颜色", type: "color" }
+]
+
+export const coArtBoardReplyTypes = {
+  agree: 'agree',
+  disagree: 'disagree',
+  busy: 'busy'
 }
