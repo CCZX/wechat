75a386894fa241005a9ef8e79eee2689d2d60099
CCZX <1831058205@qq.com>
2020-02-08 20:49:06 +0800

    server: 增加朋友圈发布以及获取好友朋友圈API

--- chatServer/controller/pyqNews.js
+++ chatServer/controller/pyqNews.js
@@ -1,10 +1,11 @@
 const PYQ_NEWS = require('./../models/pyqNews')
+const FRIEND = require('./../models/friendly')
 
 // 用户在朋友圈发表动态
 const publishPyqNews = async (req, res) => {
-  const { userId, createDate, content } = req.body
+  const { userId, createDate, content, pictures } = req.body
   const data = {
-    userId, createDate, content
+    userId, createDate, content, pictures
   }
   try {
     const doc = await PYQ_NEWS.insertMany(data)
@@ -22,6 +23,41 @@ const publishPyqNews = async (req, res) => {
   }
 }
 
+const getMyFriendPyqNews = async (req, res) => {
+  const { id } = req.query
+  const friendlyList = []
+  FRIEND.findFriendByUserM(id).then(userM => {
+    FRIEND.findFriendByUserY(id).then(userY => {
+      userM.forEach(v => {
+        friendlyList.push({
+          _id: v.userY._id
+        })
+      })
+      userY.forEach(v => {
+        friendlyList.push({
+          _id: v.userM._id
+        })
+      })
+      const friendlyIdList = friendlyList.map(item => item._id)
+      friendlyIdList.push(id)
+      PYQ_NEWS.findUserPyq(id, friendlyIdList).then(doc => {
+        return res.json({
+          status: 2000,
+          data: doc,
+          msg: '获取好友朋友圈成功！'
+        })
+      })
+    })
+  }).catch(err => {
+    return res.json({
+      status: 2003,
+      data: err,
+      msg: '服务端错误'
+    })
+  })
+}
+
 module.exports = {
-  publishPyqNews
+  publishPyqNews,
+  getMyFriendPyqNews
 }
