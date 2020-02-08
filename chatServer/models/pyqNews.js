84deec681d988a34e04914e18d10b9b8338a4395
CCZX <1831058205@qq.com>
2020-02-08 20:49:31 +0800

    server: 增加朋友圈模型

---
+++ chatServer/models/pyqNews.js
@@ -0,0 +1,41 @@
+// 用户朋友圈的数据模型
+const DB = require('./../utils/connectDB')
+const Schema = DB.Schema
+
+const pyqNewsSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  createDate: {
+    type: String
+  },
+  content: {
+    type: String,
+    required: true,
+    required: true
+  },
+  pictures: {
+    type: Array,
+     default: []
+  },
+  readCount: {
+    type: Number,
+    default: 0
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
+})
+
+pyqNewsSchema.statics.findUserPyq = function (userId, ids) {
+  return this
+        .find({
+          userId: {$in: ids}
+        }).populate({path: 'userId', select: 'nickname photo signature'})
+}
+
+const pyqNews = DB.model('pyqNews', pyqNewsSchema)
+
+module.exports = pyqNews
