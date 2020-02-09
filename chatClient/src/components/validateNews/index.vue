a0aaafed1efac32885e79fdf81e34a55b9dad82f
CCZX <1831058205@qq.com>
2020-02-09 18:56:54 +0800

    client: 在读取验证信息后修改未读消息的数量

--- chatClient/src/components/validateNews/index.vue
+++ chatClient/src/components/validateNews/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="validatenews-com">
     <div class="wrapper">
+      <div class="title">验证消息</div>
       <div class="filter">
         <el-tag type="info">选择过滤条件：</el-tag>
         <el-select v-model="filterType" placeholder="请选择">
@@ -28,12 +29,11 @@
       </div>
       <div class="counter">
         <span>
-          一共{{validateNewsList.length}}条记录
+          一共{{outcomteList.length}}条记录
         </span>
       </div>
       <!-- <div class="validatenews-item"> -->
-      <div v-show="(item.validateType === filterType && (item.status === filterStatus || filterStatus === -1))"
-        class="validatenews-item" v-for="item in validateNewsList" :key="item.time">
+      <div class="validatenews-item" v-for="item in outcomteList" :key="item.time">
         <div class="apply-info">
           <span class="title">
             <el-tooltip class="item" effect="dark" content="点击查看用户主页" placement="top">
@@ -54,14 +54,13 @@
             placement="left"
             width="400"
             trigger="click">
-            <div class="validate-popover-body">
+            <div class="validate-popover-body" v-loading="isAdding">
               <div class="sender-info">
                 <el-avatar :size="60" :src="IMG_URL + item.senderAvatar" @error="()=>true">
                   <img src="https://cube.elemecdn.com/e/fd/0fc7d20532fdaf769a25683617711png.png"/>
                 </el-avatar>
                 <div class="info">
                   <span class="nickname">{{item.senderNickname}}</span>
-                  <!-- <span class="sign">你好世界，加油中国</span> -->
                 </div>
               </div>
               <div class="addition">
@@ -70,6 +69,7 @@
                   <span>{{item.additionMessage}}</span>
                 </div>
               </div>
+              <!-- <div class="operation"> -->
               <div class="operation" v-if="item.status === 0">
                 <el-button type="success" plain size="small" @click="agreeValidate(item)">同意</el-button>
                 <el-button type="danger" plain size="small">拒绝</el-button>
@@ -81,7 +81,7 @@
                 <el-tag type="danger">已拒绝</el-tag>
               </div>
             </div>
-            <el-button slot="reference">查看</el-button>
+            <el-button slot="reference" type="success">查看</el-button>
           </el-popover>
         </div>
       </div>
@@ -90,6 +90,7 @@
 </template>
 
 <script>
+import { SET_UNREAD_NEWS_TYPE_MAP } from '@/store/constants'
 import { validateNewsTips } from '@/const'
 export default {
   props: ['validateNewsList'],
@@ -97,15 +98,48 @@ export default {
     return {
       validateNewsTips,
       IMG_URL: process.env.IMG_URL,
-      filterType: 0,
-      filterStatus: 0
+      filterType: 0, // 0人，1群
+      filterStatus: 0, // -1全部，0未处理，1已同意，2已拒绝
+      isAdding: false
+    }
+  },
+  computed: {
+    outcomteList() {
+      return this.validateNewsList.filter(item => {
+        if (this.filterStatus === -1) {
+          return item.validateType === this.filterType
+        } else {
+          return item.status === this.filterStatus && item.validateType === this.filterType
+        }
+      })
+    },
+    systemValidateUsers() {
+      return this.$store.state.app.sysUsers.filter(item => item.code === "111111")[0]
+    },
+    userInfo() {
+      return this.$store.state.user.userInfo
     }
   },
   methods: {
     agreeValidate(item) {
       this.$socket.emit('sendAgreeFriendValidate', item)
+      this.isAdding = true
+      setTimeout(() => {
+        this.isAdding = false
+        this.$emit('changeValidateNewsStatus', item, 1)
+        this.$alert('快去主页和好友聊天吧', '添加成功', {
+          confirmButtonText: '确定'
+        });
+      }, 500)
     }
-  }
+  },
+  mounted() {
+    this.$store.dispatch('news/SET_UNREAD_NEWS', {
+      roomid: this.systemValidateUsers._id + '-' + this.userInfo._id,
+      count: 0,
+      type: SET_UNREAD_NEWS_TYPE_MAP.clear
+    })
+  },
 }
 </script>
 
@@ -139,7 +173,7 @@ export default {
   background-color: #fff;
   width: 600px;
   .wrapper {
-    padding: 0 10px;
+    padding: 10px;
     box-sizing: border-box;
     .filter {
       display: flex;
@@ -158,6 +192,9 @@ export default {
       justify-content: space-between;
       align-items: center;
       border-top: 1px solid #C0C4CC;
+      &:last-child {
+        border-bottom: 1px solid #C0C4CC;
+      }
       .apply-info {
         .title {
           line-height: 20px;
