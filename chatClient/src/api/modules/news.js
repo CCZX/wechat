7aae69167d481c533754be86e1a9536b67433a54
chenchao <chenchao@beisen.com>
2020-02-14 12:06:42 +0800

    client:获取消息分页

--- chatClient/src/api/modules/news.js
+++ chatClient/src/api/modules/news.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export default {
   getRecentNews(data) {
-    const { roomid } = data
-    return request.get(`/news/recent?roomid=${roomid}`)
+    const { roomid, page, pageSize } = data
+    return request.get(`/news/recent?roomid=${roomid}&page=${page}&pageSize=${pageSize}`)
   }
 }
