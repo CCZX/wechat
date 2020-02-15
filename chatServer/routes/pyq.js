ae2b4732a492617d0f0c224e8141cdd6adabd9d1
CCZX <1831058205@qq.com>
2020-02-16 00:05:08 +0800

    server: 添加评论点赞朋友圈的路由地址

--- chatServer/routes/pyq.js
+++ chatServer/routes/pyq.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const pyqController = require('./../controller/pyqNews')
+const pyqLikeController = require('./../controller/pyqLike')
+const pyqCommentController = require('./../controller/pyqComment')
 
 router.post('/publishpyqnews', pyqController.publishPyqNews)
 router.get('/getfridenlypyq', pyqController.getMyFriendPyqNews)
+router.post('/like', pyqLikeController.doLike)
+router.post('/comment', pyqCommentController.doComment)
 
 module.exports = router
