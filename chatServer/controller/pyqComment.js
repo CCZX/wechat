5987b5b2002b06c04d48e0895ae0dd501d7823d0
CCZX <1831058205@qq.com>
2020-02-16 00:06:41 +0800

    server: 添加评论接口

---
+++ chatServer/controller/pyqComment.js
@@ -0,0 +1,31 @@
+const PYQ_COMMENT = require('./../models/pyqComment')
+
+// 用户添加评论
+const doComment = (req, res) => {
+  const { pyqId, content, authorId, parentId = '', level = 0 } = req.body
+  console.log(req.body)
+  const newComment = {
+    pyqId,
+    content,
+    authorId,
+    parentId,
+    level
+  }
+  PYQ_COMMENT.insertMany(newComment).then(doc => {
+    return res.json({
+      status: 2000,
+      data: doc,
+      msg: '评论成功'
+    })
+  }).catch(err => {
+    return res.json({
+      status: 2003,
+      data: err,
+      msg: '服务端错误'
+    })
+  })
+}
+
+module.exports = {
+  doComment
+}
