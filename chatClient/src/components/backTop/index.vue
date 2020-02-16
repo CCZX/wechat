e6538637bbcd529e6defbc9c7f84ca96422706f1
CCZX <1831058205@qq.com>
2020-02-16 18:01:35 +0800

    client: 添加BackTop组件

---
+++ chatClient/src/components/backTop/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="back-top-com">
+    <div class="back-top-com-wrapper" v-if="show" @click="goTop" title="Back Top">
+      <i class="el-icon-arrow-up"></i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    goTop() {
+      document.documentElement.scrollTop = 0
+    },
+    watchDocmentScroll() {
+      const scrollTop = document.documentElement.scrollTop
+      if (scrollTop > 2000) {
+        this.show = true
+      } else {
+        this.show = false
+      }
+    }
+  },
+  mounted() {
+    document.addEventListener('scroll', this.watchDocmentScroll)
+    this.watchDocmentScroll()
+  },
+  beforeDestroy() {
+    document.removeEventListener('scroll', this.watchDocmentScroll)
+  },
+}
+</script>
+
+<style lang="scss">
+.back-top-com {
+  position: fixed;
+  right: 20px;
+  bottom: 40px;
+  .back-top-com-wrapper {
+    cursor: pointer;
+    background-color: #fff;
+    font-size: 30px;
+  }
+}
+</style>
+
