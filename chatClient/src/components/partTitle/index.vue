c5bfc0a43c1e17e6153c64a031e7e9bf5bae93df
CCZX <1831058205@qq.com>
2020-02-22 22:02:15 +0800

    client:添加parttitle组件

---
+++ chatClient/src/components/partTitle/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="part-title-cmp">
+    <span class="part-bg" :style="'background-color:' + bgc"></span>
+    <span class="part-text">{{text}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    bgc: {
+      type: String,
+      default() {
+        return '#1890ff'
+      }
+    },
+    text: {
+      type: String,
+      default() {
+        return 'partTitle'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.part-title-cmp {
+  margin: 10px 0;
+  display: flex;
+  align-items: center;
+  .part-bg {
+    display: inline-block;
+    margin-right: 10px;
+    width: 5px;
+    height: 20px;
+    background-color: #fff;
+  }
+}
+</style>
