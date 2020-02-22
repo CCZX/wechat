8101ea3e8264212bacde3eeeb01a07657324a41f
CCZX <1831058205@qq.com>
2020-02-22 15:37:41 +0800

    server:pyqComment实体添加注释和replyToAuthorInfo字段

--- chatServer/models/pyqComment.js
+++ chatServer/models/pyqComment.js
@@ -3,7 +3,7 @@ const DB = require('./../utils/connectDB')
 const Schema = DB.Schema
 
 const pyqCommentSchema = new Schema({
-  pyqId: { // 朋友圈id
+  pyqId: { // 该评论对应的朋友圈id
     type: Schema.Types.ObjectId,
     ref: 'pyqNews'
   },
@@ -19,13 +19,25 @@ const pyqCommentSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'user',
   },
-  parentId: {
+  parentId: { // 父评论，比如B评论是回复A评论的，那么A评论就是父评论
     type: String,
     default: ''
   },
-  level: { // 评论的层级，比如直接评论就是0，评论别人的评论就是1
+  /**
+   * 评论的层级，比如对朋友圈的直接评论level为0，回复直接评论level为1，回复对直接评论的评论为2
+   */
+  level: {
     type: Number,
     default: 0
+  },
+  /**
+   * 用户A发表评论P2评论了朋友圈的直接评论P1，replyToAuthorInfo就是直接评论P1的用户信息
+   * 用户B发表评论P3评论了用户A的评论P2，replyToAuthorInfo就是用户A的信息，
+   * 需要注意的是P2评论以及P3评论的parentId都是P1的Id
+   */
+  replyToAuthorInfo: {
+    type: Object,
+    default: {}
   }
 })
 
