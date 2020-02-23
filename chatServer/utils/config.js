3d903e8f202799773a75c1b0b0cc2ccdf99b0962
CCZX <1831058205@qq.com>
2020-02-23 15:57:34 +0800

    server: 配置请求白名单

--- chatServer/utils/config.js
+++ chatServer/utils/config.js
@@ -1,9 +1,15 @@
+const { API_VERSION } = require('./../const')
 const ACCESS_KEY = 'frWfpDLauQSLS9pLI5fIQEGTxMFnpqfHCrZXn1da'
 const SECRET_KEY = 'otLFopTxILdZf8KX9hg1TCTUB1hXj9_SdhKrq1P_'
 const BUCKET = 'chenchao'
+const TOKEN_SECRET = 'ronaldo'
+const WHITE_LIST = [`${API_VERSION}/user/login`, `${API_VERSION}/user/register`, `${API_VERSION}/user/getcode`]
+
 
 module.exports = {
   ACCESS_KEY,
   SECRET_KEY,
-  BUCKET
+  BUCKET,
+  TOKEN_SECRET,
+  WHITE_LIST
 }
