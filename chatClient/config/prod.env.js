ebdc0bbc6a67887ab2d03380e5e78a7134e7f7b6
CCZX <1831058205@qq.com>
2020-03-06 13:37:07 +0800

    client:modify 线上地址

--- chatClient/config/prod.env.js
+++ chatClient/config/prod.env.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  IMG_URL: 'http://co-messager.chenr.cn',
-  SOCKET_URL: 'http://co-messager.chenr.cn',
+  IMG_URL: '',
+  SOCKET_URL: '',
 }
