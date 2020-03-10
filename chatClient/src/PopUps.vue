33848372e4b1444644b50013e181e8f13f6fb777
CCZX <1831058205@qq.com>
2020-03-10 20:05:00 +0800

    client:PopUps新的modal框

--- chatClient/src/PopUps.vue
+++ chatClient/src/PopUps.vue
@@ -6,17 +6,23 @@
       :current-conversation="currentConversation"
       @hidden-fenzu="hiddenFenzuModal"
     />
+    <beizhu-modal
+      v-if="isShowBeizhuModal"
+      :current-conversation="currentConversation"
+    />
   </div>
 </template>
 
 <script>
 import userProfile from '@/components/userProfile'
 import fenzuModal from '@/components/fenzuModal'
+import beizhuModal from '@/components/beizhuModal'
 export default {
   data() {
     return {
       isShowUserProfile: false,
       isShowFenzuModal: false,
+      isShowBeizhuModal: false,
       currentConversation: {}
     }
   },
@@ -32,12 +38,18 @@ export default {
     this.$eventBus.$on('toggleFenzuModal', (e) => {
       const { show, data } = e
       this.isShowFenzuModal = show
-      this.currentConversation = data.currentConversation
+      this.currentConversation = data.currentConversation || {}
+    })
+    this.$eventBus.$on('toggleBeizhuModal', (e) => {
+      const { show, data } = e
+      this.isShowBeizhuModal = show
+      this.currentConversation = data.currentConversation || {}
     })
   },
   components: {
     userProfile,
-    fenzuModal
+    fenzuModal,
+    beizhuModal
   }
 }
 </script>
