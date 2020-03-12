b4e7edae111f41f81c9390395d066e92d19566ca
CCZX <1831058205@qq.com>
2020-03-12 22:33:59 +0800

    server: WHITE_LIST 添加根路径

--- chatServer/utils/config.js
+++ chatServer/utils/config.js
@@ -3,7 +3,7 @@ const ACCESS_KEY = 'frWfpDLauQSLS9pLI5fIQEGTxMFnpqfHCrZXn1da'
 const SECRET_KEY = 'otLFopTxILdZf8KX9hg1TCTUB1hXj9_SdhKrq1P_'
 const BUCKET = 'chenchao'
 const TOKEN_SECRET = 'ronaldo'
-const WHITE_LIST = [`${API_VERSION}/user/login`, `${API_VERSION}/user/register`, `${API_VERSION}/user/getcode`]
+const WHITE_LIST = [`${API_VERSION}/user/login`, `${API_VERSION}/user/register`, `${API_VERSION}/user/getcode`, `/`]
 
 
 module.exports = {
