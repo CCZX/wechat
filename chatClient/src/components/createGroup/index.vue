fe7e7a1a801e0335cf3f0eaa2b7b91617eadaadb
CCZX <1831058205@qq.com>
2020-03-29 13:07:56 +0800

    client:创建群聊时选择头像

--- chatClient/src/components/createGroup/index.vue
+++ chatClient/src/components/createGroup/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="create-group-cmp">
     <div class="create-content">
-      <el-form label-position="right" :model="groupForm" :rules="rules" ref="groupForm">
+      <avatar-choose
+        v-if="showChooseAvatar"
+        @close="setShowChooseAvatar(false)"
+        @choose="chooseAvatar"
+      />
+      <el-form label-position="top" :model="groupForm" :rules="rules" ref="groupForm">
+        <el-form-item label="群头像（点击头像可切换）" required class="avatar-form">
+          <el-avatar
+            :size="50"
+            :src="IMG_URL + groupForm.avatar"
+            @click.native="setShowChooseAvatar(true)"
+          />
+        </el-form-item>
         <el-form-item label="群名称" prop="title" required>
           <el-input v-model="groupForm.title" autocomplete="on"></el-input>
         </el-form-item>
@@ -18,12 +30,14 @@
 </template>
 
 <script>
+import avatarChoose from '@/components/avatarChoose'
 export default {
   data() {
     return {
       groupForm: {
         title: '',
-        desc: ''
+        desc: '',
+        avatar: 'face/face1.jpg'
       },
       rules: {
         title: [
@@ -33,7 +47,9 @@ export default {
         desc: [
           { min: 8, max: 20, message: '长度在 8 到 20 个字符', trigger: 'blur' }
         ]
-      }
+      },
+      showChooseAvatar: false,
+      IMG_URL: process.env.IMG_URL
     }
   },
   computed: {
@@ -46,7 +62,7 @@ export default {
       const params = {
         title: this.groupForm.title,
         desc: this.groupForm.desc,
-        img: '',
+        img: this.groupForm.avatar,
         holderName: this.userInfo.name,
         holderUserId: this.userInfo._id
       }
@@ -73,7 +89,26 @@ export default {
     },
     resetForm(formName) {
       this.$refs[formName].resetFields()
+    },
+    setShowChooseAvatar(flag) {
+      this.showChooseAvatar = flag
+    },
+    chooseAvatar(item) {
+      this.groupForm.avatar = item
     }
   },
+  components: {
+    avatarChoose
+  }
 }
 </script>
+
+<style lang="scss">
+.create-group-cmp {
+  .create-content {
+    .avatar-form {
+      margin: 0;
+    }
+  }
+}
+</style>
