8ac94c75f4b8e72e7f4ba2a063c03338317d2ec0
CCZX <1831058205@qq.com>
2020-03-31 15:05:58 +0800

    client:缺失组件上传（未开发完成）

---
+++ chatClient/src/components/userProfile/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="user-profile all0">
+    <p>
+      userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    userProfile
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+.user-profile {
+  position: fixed;
+  background-color: rgba(0, 0, 0, .2);
+  z-index: 1007;
+  text-align: center;
+}
+</style>
+
